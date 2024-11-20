24777359368d079eff9b436b6c7e0218fa62fc9f
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2024-11-20 00:26:44 -0300

    fix: db cleaned

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,79 +1,8 @@
 const db: DataBase = {
-    users: [
-        {
-            id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            first_name: "Rafhael",
-            last_name: "Mallorga",
-            email: "rafhaelmallorga@gmail.com",
-            password: "Rafha@1234",
-        }
-    ],
-    bank_accounts: [
-        {
-            id: "acd2scab-f1924566837a-ef5173000abc",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            name: "Itau",
-            amount: 1000,
-        },
-        {
-            id: "acd27cab-f1924566837a-ef5173000abc",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            name: "NuBank",
-            amount: 1500,
-        },
-        {
-            id: "acd2rcab-f1924566837a-ef5173000abc",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            name: "BB",
-            amount: 2200,
-        }
-    ],
-    savings: [
-        {
-            id: "8696f5ee-5a3a-4ef5-b9dc-89375b92b31d",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            name: "Travel",
-            amount: 100,
-        },
-        {
-            id: "8696f5ae-5a3a-4ef5-b9dc-89375b92b31d",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            name: "New Car",
-            amount: 230,
-        },
-        {
-            id: "86s6f5ae-5a3a-4ef5-b9dc-89375b92b31d",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            name: "New House",
-            amount: 2300,
-        }
-    ],
-    transactions: [
-        {
-            id: "9d0088ab-fba3-4b8b-92d0-71a57b9fc87f",
-            title: "Gas",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            type: "outcome",
-            date: "",
-            value: 200
-        },
-        {
-            id: "9d0088ab-fb33-4b8b-92d0-71a57b9fc87f",
-            title: "Card",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            type: "outcome",
-            date: "",
-            value: 2300
-        },
-        {
-            id: "9d0088ab-fb32-4b8b-92d0-71a57b9fc87f",
-            title: "Salary",
-            user_id: "5af2c19b-0a8b-47a5-9e7a-4ccb6e89b029",
-            type: "income",
-            date: "",
-            value: 2300
-        },
-    ]
+    users: [],
+    bank_accounts: [],
+    savings: [],
+    transactions: []
 }
 
 type DataBase = {
